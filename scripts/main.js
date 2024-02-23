c1b258324d4b42cc40ff723892671c6b34dc186f
Rastaw <ashumarks@gmail.com>
2024-02-23 19:28:02 +0100

    github pages Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,37 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if(mySrc === "images/node-ninja-logo.png") {
+    myImage.setAttribute("src", "images/node-ninja-js.png");
+  } else {
+    myImage.setAttribute("src", "images/node-ninja-logo.png");
+  }
+};
+
+let myButton = document.querySelector("button");
+let myHeader = document.querySelector("h1");
+
+//Now the function
+
+function setUserName() {
+  const myName = prompt("please Enter your Name");
+  if(!myName) {
+    setUserName();
+  } else{
+    localStorage.setItem("name", myName);
+    myHeader.textContent = `Node Ninja is cool, ${myName}`;
+  }
+  
+}
+
+if(!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeader.textContent = `Ninja is great, ${storedName}`;
+}
+
+myButton.onclick = () => {
+  setUserName();
+};
